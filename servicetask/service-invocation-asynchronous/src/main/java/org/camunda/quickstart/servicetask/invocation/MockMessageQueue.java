2940795f32089d2e16ca0463077c49c93f77d311
Vilmos Nagy <vilmos.nagy@outlook.com>
2018-09-17 15:11:49 +0200

    Error reproduced

--- servicetask/service-invocation-asynchronous/src/main/java/org/camunda/quickstart/servicetask/invocation/MockMessageQueue.java
+++ servicetask/service-invocation-asynchronous/src/main/java/org/camunda/quickstart/servicetask/invocation/MockMessageQueue.java
@@ -1,43 +1,50 @@
 package org.camunda.quickstart.servicetask.invocation;
 
+import java.util.Deque;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.LinkedBlockingDeque;
 
 /**
- * <p>This class is supposed to mock the queuing infrastructure between the process engine 
- * and the service implementation. In a real life scenario, we would use reliable queuing 
+ * <p>This class is supposed to mock the queuing infrastructure between the process engine
+ * and the service implementation. In a real life scenario, we would use reliable queuing
  * middleware such as a transactional messaging system (typically JMS).</p>
  *
  */
 public class MockMessageQueue {
-  
+
   /** a Message with payload */
   public static class Message {
-    
-    protected Map<String, Object> payload = new HashMap<String, Object>();
+
+    protected final Map<String, Object> payload;
 
     public Message(Map<String, Object> payload) {
       this.payload = payload;
     }
-    
+
     public Map<String, Object> getPayload() {
       return payload;
     }
-     
+
   }
-  
-  protected List<Message> queue = new LinkedList<MockMessageQueue.Message>();
-  
+
+  protected BlockingDeque<Message> queue = new LinkedBlockingDeque<>();
+
   public final static MockMessageQueue INSTANCE = new MockMessageQueue();
-  
+
   public void send(Message m) {
     queue.add(m);
   }
-  
+
   public Message getNextMessage() {
-    return queue.remove(0);
+    try {
+      return queue.take();
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    }
   }
 
 }
--- servicetask/service-invocation-asynchronous/src/test/java/org/camunda/quickstart/servicetask/invocation/sync/TestAsynchronousServiceTask.java
+++ servicetask/service-invocation-asynchronous/src/test/java/org/camunda/quickstart/servicetask/invocation/sync/TestAsynchronousServiceTask.java
@@ -8,6 +8,8 @@ import static junit.framework.Assert.fail;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
 
 import org.camunda.bpm.engine.ProcessEngine;
 import org.camunda.bpm.engine.RuntimeService;
@@ -25,7 +27,7 @@ import org.junit.Test;
 
 /**
  * Test case for demonstrating the asynchronous service invocation.
- * 
+ *
  */
 public class TestAsynchronousServiceTask {
 
@@ -34,99 +36,30 @@ public class TestAsynchronousServiceTask {
 
   @Test
   @Deployment(resources = { "asynchronousServiceInvocation.bpmn" })
-  public void testServiceInvocationSuccessful() {
+  public void testServiceInvocationSuccessful() throws InterruptedException {
 
     final ProcessEngine processEngine = processEngineRule.getProcessEngine();
     final RuntimeService runtimeService = processEngineRule.getRuntimeService();
-    final TaskService taskService = processEngineRule.getTaskService();
 
-    // this invocation should NOT fail
     Map<String, Object> variables = Collections.<String, Object> singletonMap(BusinessLogic.SHOULD_FAIL_VAR_NAME, false);
 
-    // start the process instance
-    ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("asynchronousServiceInvocation", variables);
+    // start the process instance on a new thread
+    new Thread(() -> {
+      runtimeService.startProcessInstanceByKey("asynchronousServiceInvocation", variables);
+    }).start();
 
-    // the process instance is now waiting in the first wait state (user task):
-    Task waitStateBefore = taskService.createTaskQuery()
-      .taskDefinitionKey("waitStateBefore")
-      .processInstanceId(processInstance.getId())
-      .singleResult();
-    assertNotNull(waitStateBefore);
-
-    // Complete the first task. This triggers causes the Service task to be executed. 
-    // After the method call returns, the message will be put into the queue and 
-    // the process instance is waiting in the service task activity.
-    taskService.complete(waitStateBefore.getId());
-
-    // the process instance is now waiting in the service task activity:
-    assertEquals(Arrays.asList("serviceTaskActivity"), runtimeService.getActiveActivityIds(processInstance.getId()));
-    
     // the message is present in the Queue:
+    System.out.println("Waiting for Message, " + System.currentTimeMillis());
+
     Message message = MockMessageQueue.INSTANCE.getNextMessage();
+
+    System.out.println("Message received, " + System.currentTimeMillis());
+
     assertNotNull(message);
-    assertEquals(processInstance.getId(), message.getPayload().get(AsynchronousServiceTask.EXECUTION_ID));
 
     // Next, trigger the business logic. This will send the callback to the process engine.
     // When this method call returns, the process instance will be waiting in the next waitstate.
     BusinessLogic.INSTANCE.invoke(message, processEngine);
-    
-    // the process instance is now waiting in the second wait state (user task):
-    Task waitStateAfter = taskService.createTaskQuery()
-      .taskDefinitionKey("waitStateAfter")
-      .processInstanceId(processInstance.getId())
-      .singleResult();
-    assertNotNull(waitStateAfter);
-        
-    // check for variable set by the service task:
-    variables = runtimeService.getVariables(processInstance.getId());
-    assertEquals(BusinessLogic.PRICE, variables.get(BusinessLogic.PRICE_VAR_NAME));
-
-  }
-
-  @Test
-  @Deployment(resources = { "asynchronousServiceInvocation.bpmn" })
-  public void testServiceInvocationFailure() {
-
-    final ProcessEngine processEngine = processEngineRule.getProcessEngine();
-    final RuntimeService runtimeService = processEngine.getRuntimeService();
-    final TaskService taskService = processEngine.getTaskService();
-
-    // this invocation should fail
-    Map<String, Object> variables = Collections.<String, Object> singletonMap(BusinessLogic.SHOULD_FAIL_VAR_NAME, true);
-
-    // start the process instance
-    ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("asynchronousServiceInvocation", variables);
-
-    // the process instance is now waiting in the first wait state (user task):
-    Task waitStateBefore = taskService.createTaskQuery()
-      .taskDefinitionKey("waitStateBefore")
-      .processInstanceId(processInstance.getId())
-      .singleResult();
-    assertNotNull(waitStateBefore);
-
-    // Complete the first task. This triggers causes the Service task to be executed. 
-    // After the method call returns, the message will be put into the queue and 
-    // the process instance is waiting in the service task activity.
-    taskService.complete(waitStateBefore.getId());
-
-    // the process instance is now waiting in the service task activity:
-    assertEquals(Arrays.asList("serviceTaskActivity"), runtimeService.getActiveActivityIds(processInstance.getId()));
-    
-    // the message is present in the Queue:
-    Message message = MockMessageQueue.INSTANCE.getNextMessage();
-    assertNotNull(message);
-    assertEquals(processInstance.getId(), message.getPayload().get(AsynchronousServiceTask.EXECUTION_ID));
-
-    // Next, trigger the business logic. In this case, this will throw an exception:
-    try {
-      BusinessLogic.INSTANCE.invoke(message, processEngine);
-      fail("exception expected!");
-    } catch (Exception e) {
-      assertTrue(e.getMessage().contains("Service invocation failure!"));
-    }
-
-    // the process instance is still waiting in the service task activity:
-    assertEquals(Arrays.asList("serviceTaskActivity"), runtimeService.getActiveActivityIds(processInstance.getId()));
 
   }
 
